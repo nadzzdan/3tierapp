f60d86027ad4be6abf7e245e522fa05db8ab8df1
nadzzdan <knnthss@gmail.com>
2025-09-12 16:12:57 +0800

    Delete database directory

--- database/init.sql
+++
@@ -1,6 +0,0 @@
--- Table is also created by backend ensure_table(), but we keep this for clarity
-CREATE TABLE IF NOT EXISTS texts (
-    id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
